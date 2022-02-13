8f18a3f16bceef090d3f14940f534ec9da640709
IagoFMelo <iagodanielf2@gmail.com>
2022-02-13 01:10:54 -0300

    New Version

--- Classe_Controle.py
+++ Classe_Controle.py
@@ -1,3 +1,5 @@
+# @Propriedade de Iago Melo
+
 # coding: utf-8
 
 from inspect import trace
@@ -5,12 +7,15 @@ from re import X
 from control import *
 import matplotlib.pyplot as plt
 
+sn = ["Y", "y"]
+
 
 class Transfer_function:
 
-    def __init__(self, tf1=None, tf2=None):  # Inicialização
+    def __init__(self, tf1=None, tf2=None, tff=None):  # Inicialização
         self.tf1 = tf1
         self.tf2 = tf2
+        self.tff = tff
 
     def resposta_degrau(self):
 
@@ -30,3 +35,24 @@ if __name__ == "__main__":
     s = tf("s")
     g1 = tf([1, 0], [2, 1, 2])
     print(g1)
+
+    # Usar declarador nesta função
+    def feedback_tf(self):
+
+        fb = input("o sistema tem realimentação? [Y]/[y] ou [N]/[n]: ")
+        if fb in sn:
+            fb1, fb2, fb3, fb4 = input(
+                "Digite os coeficientes do numerador da Função de Realimentação: ").split()
+            fb11, fb22, fb33, fb44 = input(
+                "Digite os coeficientes do denominador da Função de Realimentação: ").split()
+            gff = tf([int(fb1), int(fb2), int(fb3), int(fb4)], [
+                     int(fb11), int(fb22), int(fb33), int(fb44)])
+            g1y, g2y = input(
+                "A Função Transferência que está sendo realimentada é G1? É G2? (Colocar espaçado) [Y]/[y] ou [N]/[n]: ").split()
+
+            if g1y in sn:
+                fb_tf = feedback(self.tf1, gff)
+                print(fb_tf)
+            elif g2y in sn:
+                fb_tf = feedback(self.tf2, gff)
+                print(fb_tf)
--- questões_gerais.py
+++ questões_gerais.py
@@ -1,6 +1,11 @@
+# @Propriedade de Iago Melo
+
+
 # coding: utf-8
+
 from matplotlib.pyplot import get_fignums
 from Classe_Controle import *
+import sys
 
 s = tf("s")
 
@@ -9,65 +14,68 @@ sn = ["Y", "y"]
 g1 = None
 g2 = None
 
-print("A digitação da função se dará da forma -> Ex: Numerador - (sˆ2+2s) :. 0 1 2 0\n Coeficientes iniciam com sˆ3")
+while True:
+    print("A digitação da função se dará da forma -> Ex: Numerador - (sˆ2+2s) :. 0 1 2 0\nCoeficientes iniciam com sˆ3\n")
 
-x1, y1, w1, z1 = input(
-    "Digite os coeficientes do numerador da Função transferência G1: ").split()
+    x1, y1, w1, z1 = input(
+        "Digite os coeficientes do numerador da Função transferência G1: ").split()
 
-x11, y11, w11, z11 = input(
-    "Digite os coeficientes do denominador da Função transferência G1: ").split()
+    x11, y11, w11, z11 = input(
+        "Digite os coeficientes do denominador da Função transferência G1: ").split()
 
-g1 = tf([int(x1), int(y1), int(w1), int(z1)], [
-        int(x11), int(y11), int(w11), int(z11)])
+    ti_1 = input(
+        "A função transferência tem termo independente? [Y]/[y] ou [N]/[n]: ")
 
-print(g1)
-
-us = input(
-    "Será utilizada uma segunda função transferência? [Y]/[y] ou [N]/[n]: ")
-
-if us in sn:
-    x2, y2, w2, z2 = input(
-        "Digite os coeficientes do numerador da Função transferência G2: ").split()
-
-    x22, y22, w22, z22 = input(
-        "Digite os coeficientes do denominador da Função transferência G2: ").split()
-
-    ti_ = input(
-        "A função transferência tem termo independente? [Y]/[y] ou [N]/[n]:")
-
-    if ti_ in sn:
+    if ti_1 in sn:
 
         ti = input("Digite o valor do termo independete: ")
-        g2 = tf([int(x2), int(y2), int(w2), int(z2)], [
-                int(x22), int(y22), int(w22), int(z22)]) + int(ti)
+        g1 = tf([int(x1), int(y1), int(w1), int(z1)], [
+                int(x11), int(y11), int(w11), int(z11)]) + int(ti)
     else:
 
-        g2 = tf([int(x2), int(y2), int(w2), int(z2)], [
-                int(x22), int(y22), int(w22), int(z22)])
+        g1 = tf([int(x1), int(y1), int(w1), int(z1)], [
+                int(x11), int(y11), int(w11), int(z11)])
 
-print(g2)
+    print(g1)
 
+    us = input(
+        "Será utilizada uma segunda função transferência? [Y]/[y] ou [N]/[n]: ")
 
-# gf = None - Melhoria do código
+    if us in sn:
+        x2, y2, w2, z2 = input(
+            "Digite os coeficientes do numerador da Função transferência G2: ").split()
 
-realimentacao = input("o sistema tem realimentação? [Y]/[y] ou [N]/[n]: ")
+        x22, y22, w22, z22 = input(
+            "Digite os coeficientes do denominador da Função transferência G2: ").split()
 
-if realimentacao in sn:
-    x = input("Digite a função transferência do numerador da realimentação: ")
-    gr = input("Digite a função transferência do denominador de realimentação: ")
-    gf = feedback(x, gr, -1)
+        ti_2 = input(
+            "A função transferência tem termo independente? [Y]/[y] ou [N]/[n]:")
 
+        if ti_2 in sn:
 
-# if gf is not None:
-#    y4, t4 = step_response(gf)
+            ti1 = input("Digite o valor do termo independete: ")
+            g2 = tf([int(x2), int(y2), int(w2), int(z2)], [
+                    int(x22), int(y22), int(w22), int(z22)]) + int(ti1)
+        else:
 
+            g2 = tf([int(x2), int(y2), int(w2), int(z2)], [
+                    int(x22), int(y22), int(w22), int(z22)])
 
-controle1 = Transfer_function(g1, g2)
-controle1.resposta_degrau()
-tit = input("Digite qual o título do seu gráfico: ")
-plt.title(tit)
-plt.legend(fontsize=12)
-plt.xlabel("Tempo[t]")
-plt.ylabel("y(t)")
-plt.grid()
-plt.show()
+    print(g2)
+
+    controle1 = Transfer_function(g1, g2, )
+    controle1.feedback_tf()
+    controle1.resposta_degrau()
+    tit = input("Digite qual o título do seu gráfico: ")
+    plt.title(tit)
+    plt.legend(fontsize=12)
+    plt.xlabel("Tempo[t]")
+    plt.ylabel("y(t)")
+    plt.grid()
+    plt.show()
+
+    answer = input("Rodar novamente? [Y]/[y] ou [N]/[n]: ")
+    if answer in sn:
+        print("Continua...")
+    else:
+        sys.exit()
